f1494e734a9970383f254dc1ebd75403cbe57673
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-10-24 21:00:24 +0530

    updates

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var express = require('express');//create an Express class
 var primary_app_object = express();//create an instance of Express class into an object
 primary_app_object.set('view engine', 'jade');// Set the view engine
 primary_app_object.set('views', './views');// Where to find the view files
-
+primary_app_object.use(express.static('./public'));//where to find static files
 //initial route for app
 primary_app_object.get('/', function (req, res) {
   res.render('index');
@@ -23,5 +23,5 @@ primary_app_object.get('/login', function (req, res) {
 
 //server function for the primary_app_object
 primary_app_object.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
