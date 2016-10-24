01a2c23f1417e2ca0bd64477547f1c0c0d63b308
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-10-24 19:52:44 +0530

    main file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var express = require('express');
-var app = express();
+//Project Image
+//Written by Sandeep Vattapparambil
+//sandeepv68@gmail.com
 
-app.get('/', function (req, res) {
+//Use strict syntax and semantics
+'use strict';
+//create an Express class
+var express = require('express');
+//create an instance of Express class into an object
+var primary_app_object = express();
+//initial route for app
+primary_app_object.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
-app.listen(3000, function () {
+//server function for the primary_app_object 
+primary_app_object.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
