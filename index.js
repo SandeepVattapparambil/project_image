29d1a004f4dbcf90f2036fe00e9723dabe83fe2e
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-10-24 20:21:54 +0530

    updates

--- index.js
+++ index.js
@@ -2,16 +2,28 @@
 //Written by Sandeep Vattapparambil
 //sandeepv68@gmail.com
 
-//Use strict syntax and semantics
-'use strict';
-//create an Express class
-var express = require('express');
-//create an instance of Express class into an object
-var primary_app_object = express();
+'use strict';//Use strict syntax and semantics
+var http = require('http');//Include the Node HTTP library
+var express = require('express');//create an Express class
+
+var primary_app_object = express();//create an instance of Express class into an object
+primary_app_object.set('view engine', 'jade');// Set the view engine
+primary_app_object.set('views', './views');// Where to find the view files
+
 //initial route for app
 primary_app_object.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
+primary_app_object.get('/login', function (req, res) {
+  res.send('Hello World! login');
+});
+primary_app_object.get('/home', function (req, res) {
+  res.send('Hello World! home');
+});
+primary_app_object.get('/about', function (req, res) {
+  res.send('Hello World! about');
+});
+
 //server function for the primary_app_object
 primary_app_object.listen(3000, function () {
   console.log('Example app listening on port 3000!');
